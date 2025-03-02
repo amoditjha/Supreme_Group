882961c069984f626facfbe24c256b0817021619
amoditjha <amodit1301@gmail.com>
2025-03-02 21:37:14 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import ContactForm from "@/Components/ContactForm";
 import Content from "@/Components/Content";
 import Hero from "@/Components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
